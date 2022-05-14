2dcb728a91894dc3a20720d116ecf7a79b68a2b9
Ataraxia725 <sanciyuanzbg@gmail.com>
2022-05-14 13:05:39 +0800

    init

---
+++ src/main/java/edu/sicau/SpringBootApplicationn.java
@@ -0,0 +1,9 @@
+package edu.sicau;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class SpringBootApplicationn {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApplicationn.class);
+    }
+}
---
+++ src/main/java/edu/sicau/controller/LoginController.java
@@ -0,0 +1,27 @@
+package edu.sicau.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import javax.servlet.HttpConstraintElement;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Objects;
+@Controller
+public class LoginController {
+   @RequestMapping( value = "login",method = RequestMethod.POST  )
+    public void doLogin(HttpServletRequest request ,HttpServletResponse response) throws IOException, ServletException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if(username.isEmpty()||password.isEmpty()){
+            request.getRequestDispatcher("login.html").forward(request,response);
+        }else if("yjl".equals(username) && "123".equals(password)){
+            response.sendRedirect("b.html");
+        }else{
+            response.sendRedirect("login.html");
+        }
+    }
+}
